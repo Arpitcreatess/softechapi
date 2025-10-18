217d7194534db42bf21207add1aa1ea42ba642ff
Arpitcreatess <arpitcreates23@gmail.com>
2025-10-18 13:34:29 +0530

    Update db.js

--- Daabase/db.js
+++ Daabase/db.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017mongodb+srv://arpit:Password123@cluster0.wtechb0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const url = 'mongodb+srv://arpit:password123jk@cluster0.wtechb0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const client = new MongoClient(url);
-const dbName = 'local';
+const dbName = 'sample_mflix';
 
 async function main() {
     // Use connect method to connect to the server
